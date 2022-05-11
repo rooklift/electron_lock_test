f23fbd43734b044afaec273a50e7a5527d8e3a28
rooklift <16438795+rooklift@users.noreply.github.com>
2022-05-11 22:12:16 +0100

    try crashReporter

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ if (!electron.app.requestSingleInstanceLock()) {
 	console.log("Lock Test is apparently already running. Shutting down this instance.");
 	electron.app.quit();
 } else {
+	console.log("crashDumps at:", electron.app.getPath("crashDumps"));
+	electron.crashReporter.start({submitURL: "", uploadToServer: false});
 	electron.app.whenReady().then(() => {
 		win = new electron.BrowserWindow();
 		win.loadFile(path.join(__dirname, "renderer.html"));
